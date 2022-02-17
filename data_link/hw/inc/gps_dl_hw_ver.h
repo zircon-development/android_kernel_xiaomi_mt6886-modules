650f89ece18665de2cc7a741beae1d3123e27004
Zhenyu Jing <zhenyu.jing@mediatek.com>
2022-02-17 15:38:14 +0800

    [ALPS05554631] GPS: Update MT6877 POS
    Update MT6877 POS
    
    MTK-Commit-Id: 55cf3f665584115736f2b6d3ac79b683afe74be9
    
    Signed-off-by: Zhenyu jing <zhenyu.jing@mediatek.com>
    CR-Id: ALPS05554631
    Feature: Location Chipset Capability
    Change-Id: Id3f658e36186369337c469c1ed9777f8c6ce7421

--- data_link/hw/inc/gps_dl_hw_ver.h
+++ data_link/hw/inc/gps_dl_hw_ver.h
@@ -12,7 +12,7 @@
 
 
 #define GDL_HW_BGF_VER_CONNAC2_0     (0x20010000)
-#define GDL_HW_BGF_VER_MT6877        (0x20040200)
+#define GDL_HW_BGF_VER_MT6877        (0x02040200)
 
 
 #endif /* _GPS_DL_HW_VER_H */
