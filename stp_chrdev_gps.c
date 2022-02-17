55132225b8691d764b4d105e4c3d148ca12c4389
Jianping Jiang <jianping.jiang@mediatek.com>
2022-02-17 15:34:07 +0800

    [ALPS04978264] GPS: Change usage of API wakeup_source_register
    wakeup_source_register (struct device *dev, const char *name)
    change API according to google
    1. wakeup_source_init to be wakeup_source_register
    2. wakeup_source_trash to be wakeup_source_unregister
    
    Conflicts:
    	stp_chrdev_gps.c
    
    MTK-Commit-Id: dee635ef399a1fb5da11996314bc9622fa5866e7
    
    Change-Id: I1b655b857165846aff915f572702fa25250eb50e
    Signed-off-by: Jianping Jiang <jianping.jiang@mediatek.com>
    CR-Id: ALPS04978264
    Feature: Location Chipset Capability
    (cherry picked from commit 3b6f30d9cd79a29a83124f84f9f94cdb984598ce)

--- stp_chrdev_gps.c
+++ stp_chrdev_gps.c
@@ -44,6 +44,7 @@
  *#include <helio-dvfsrc.h>
  */
 #endif
+#include <linux/version.h>
 
 MODULE_LICENSE("GPL");
 
@@ -121,7 +122,7 @@ static int GPS_major = GPS_DEV_MAJOR;	/* dynamic allocation */
 module_param(GPS_major, uint, 0);
 static struct cdev GPS_cdev;
 
-static struct wakeup_source gps_wake_lock;
+static struct wakeup_source *gps_wake_lock_ptr;
 static unsigned char wake_lock_acquired;   /* default: 0 */
 
 #if (defined(CONFIG_MTK_GMO_RAM_OPTIMIZE) && !defined(CONFIG_MTK_ENG_BUILD))
@@ -206,7 +207,7 @@ static void gps_hold_wake_lock(int hold)
 	if (hold == 1) {
 		if (!wake_lock_acquired) {
 			GPS_DBG_FUNC("acquire gps wake_lock acquired = %d\n", wake_lock_acquired);
-			__pm_stay_awake(&gps_wake_lock);
+			__pm_stay_awake(gps_wake_lock_ptr);
 			wake_lock_acquired = 1;
 		} else {
 			GPS_DBG_FUNC("acquire gps wake_lock acquired = %d (do nothing)\n", wake_lock_acquired);
@@ -214,7 +215,7 @@ static void gps_hold_wake_lock(int hold)
 	} else if (hold == 0) {
 		if (wake_lock_acquired) {
 			GPS_DBG_FUNC("release gps wake_lock acquired = %d\n", wake_lock_acquired);
-			__pm_relax(&gps_wake_lock);
+			__pm_relax(gps_wake_lock_ptr);
 			wake_lock_acquired = 0;
 		} else {
 			GPS_DBG_FUNC("release gps wake_lock acquired = %d (do nothing)\n", wake_lock_acquired);
@@ -1165,8 +1166,15 @@ static int GPS_init(void)
 		goto error;
 #endif
 	pr_warn("%s driver(major %d) installed.\n", GPS_DRIVER_NAME, GPS_major);
-
-	wakeup_source_init(&gps_wake_lock, "gpswakelock");
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 83)
+	gps_wake_lock_ptr = wakeup_source_register(NULL, "gpswakelock");
+#else
+	gps_wake_lock_ptr = wakeup_source_register("gpswakelock");
+#endif
+	if (!gps_wake_lock_ptr) {
+		pr_info("%s %d: init wakeup source fail!", __func__, __LINE__);
+		goto error;
+	}
 
 	sema_init(&status_mtx, 1);
 	sema_init(&fwctl_mtx, 1);
@@ -1209,7 +1217,7 @@ static void GPS_exit(void)
 	unregister_chrdev_region(dev, GPS_devs);
 	pr_warn("%s driver removed.\n", GPS_DRIVER_NAME);
 
-	wakeup_source_trash(&gps_wake_lock);
+	wakeup_source_unregister(gps_wake_lock_ptr);
 }
 
 int mtk_wcn_stpgps_drv_init(void)
