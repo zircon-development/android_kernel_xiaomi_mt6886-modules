2d6957168d5c31abea80e5e3b977679e53158f80
Tianfang Li <tianfang.li@mediatek.com>
2022-02-17 15:41:18 +0800

    [ALPS05728962] GPS : Add gps_pwr dev for power throttling
    Add gps_pwr dev for power throttling
    
    MTK-Commit-Id: 1ab52730f00ed175d57c9143258b912867010880
    
    Change-Id: I399cd0b3e4a3954f720466dadeb75e2634d11869
    Signed-off-by: Tianfang Li <tianfang.li@mediatek.com>
    CR-Id: ALPS05728962
    Feature: Location Chipset Capability

--- gps_pwr/Android.mk
+++ gps_pwr/Android.mk
@@ -4,10 +4,10 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(MTK_GPS_SUPPORT), yes)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := gps_scp.ko
+LOCAL_MODULE := gps_pwr.ko
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_OWNER := mtk
-LOCAL_INIT_RC := init.gps_scp.rc
+LOCAL_INIT_RC := init.gps_pwr.rc
 
 include $(MTK_KERNEL_MODULE)
 
