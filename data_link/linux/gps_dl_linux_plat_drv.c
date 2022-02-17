7e980a8092f61c60f7a99ad03611911a149d4c10
Zhenyu Jing <zhenyu.jing@mediatek.com>
2022-02-17 15:43:12 +0800

    [ALPS05681726] GPS : mt6895 early porting -- ids
    GPS : mt6895 early porting -- ids
    
    MTK-Commit-Id: c82c69e9d109e2357134a9e6bd07f680e97aaba2
    
    Signed-off-by: Zhenyu Jing <zhenyu.jing@mediatek.com>
    CR-Id: ALPS05681726
    Feature: Location Chipset Capability
    Change-Id: I2f3edc984440c884fc93e67a319127120b1fab8b

--- data_link/linux/gps_dl_linux_plat_drv.c
+++ data_link/linux/gps_dl_linux_plat_drv.c
@@ -41,6 +41,7 @@ const struct of_device_id gps_dl_of_ids[] = {
 	{ .compatible = "mediatek,mt6877-gps", },
 	{ .compatible = "mediatek,mt6983-gps", },
 	{ .compatible = "mediatek,mt6879-gps", },
+	{ .compatible = "mediatek,mt6895-gps", },
 	{},
 };
 /* #endif */
