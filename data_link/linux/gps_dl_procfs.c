c3fbdbf63756f9cf1ee011cee068bb779c5b37a5
Hua Fu <hua.fu@mediatek.com>
2022-02-17 15:31:56 +0800

    [ALPS04088869] GPS: Can set log level by gpsdl_dbg
    Can set log level by gpsdl_dbg.
    
    MTK-Commit-Id: 45c6fff3f466f7a92dad9e1e623ba279deeafba1
    
    Change-Id: I11e4407f14e069f42afc681d9c05e1b4344a511b
    Signed-off-by: Hua Fu <hua.fu@mediatek.com>
    CR-Id: ALPS04088869
    Feature: Location Chipset Capability

--- data_link/linux/gps_dl_procfs.c
+++ data_link/linux/gps_dl_procfs.c
@@ -20,39 +20,61 @@
 #include "gps_each_link.h"
 #include "gps_dl_subsys_reset.h"
 
-int gps_dl_procfs_trigger_reset(int par1, int par2, int par3)
+int gps_dl_procfs_dummy_op(int y, int z)
 {
-	if (par2 == 0)
+	GDL_LOGW("do nothing: y = %d, z = %d", y, z);
+	return 0;
+}
+
+int gps_dl_procfs_set_opt(int y, int z)
+{
+	if (y == 1) {
+		enum gps_dl_log_level_enum level_old, level_new;
+
+		level_old = gps_dl_log_level_get();
+		gps_dl_log_level_set(z);
+		level_new = gps_dl_log_level_get();
+		GDL_LOGW("log level change: %d to %d", level_old, level_new);
+	}
+
+	return 0;
+}
+
+int gps_dl_procfs_trigger_reset(int y, int z)
+{
+	if (y == 0)
 		gps_dl_trigger_connsys_reset();
-	else if (par2 == 1)
-		gps_dl_trigger_gps_subsys_reset((bool)par3);
-	else if (par2 == 2 && (par3 >= 0 && par3 <= GPS_DATA_LINK_NUM))
-		gps_each_link_reset(par3);
+	else if (y == 1)
+		gps_dl_trigger_gps_subsys_reset((bool)z);
+	else if (y == 2 && (z >= 0 && z <= GPS_DATA_LINK_NUM))
+		gps_each_link_reset(z);
 
 	return 0;
 }
 
 gps_dl_procfs_test_func_type g_gps_dl_proc_test_func_list[] = {
-	[0x00] = gps_dl_procfs_trigger_reset,
+	[0x00] = gps_dl_procfs_dummy_op,
+	/* [0x01] = TODO: reg read */
+	/* [0x02] = TODO: reg write */
+	[0x03] = gps_dl_procfs_set_opt,
+	[0x04] = gps_dl_procfs_trigger_reset,
 };
 
 #define UNLOCK_MAGIC 0xDB9DB9
 #define PROCFS_WR_BUF_SIZE 256
 ssize_t gps_dl_procfs_write(struct file *filp, const char __user *buffer, size_t count, loff_t *f_pos)
 {
-	size_t len = count;
+	size_t len = count, sub_len;
 	char buf[PROCFS_WR_BUF_SIZE];
 	char *pBuf;
-	char *pDelimiter = " \t";
 	int x = 0, y = 0, z = 0;
 	char *pToken = NULL;
 	long res;
-
-	/* static bool testEnabled; */
+	static bool gpsdl_dbg_enabled;
 
 	GDL_LOGD("write parameter len = %d", (int)len);
 	if (len >= PROCFS_WR_BUF_SIZE) {
-		GDL_LOGE("input handling fail!\n");
+		GDL_LOGE("input handling fail!");
 		return -1;
 	}
 
@@ -60,57 +82,67 @@ ssize_t gps_dl_procfs_write(struct file *filp, const char __user *buffer, size_t
 		return -EFAULT;
 
 	buf[len] = '\0';
-	GDL_LOGD("write parameter data = %s", buf);
-
 	pBuf = buf;
-	pToken = osal_strsep(&pBuf, pDelimiter);
-	if (pToken != NULL) {
-		osal_strtol(pToken, 16, &res);
-		x = (int)res;
-	} else {
-		x = 0;
-	}
+	do {
+		res = 0;
+		sub_len = strlen(pBuf);
+		GDL_LOGD("write parameter data = %s, len = %d", pBuf, sub_len);
+		if (sub_len != 0)
+			pToken = osal_strsep(&pBuf, "\t\n\r ");
+		if (sub_len != 0 && pToken != NULL) {
+			osal_strtol(pToken, 16, &res);
+			x = (int)res;
+		} else {
+			GDL_LOGW("x use default value");
+			break;
+		}
 
-	pToken = osal_strsep(&pBuf, "\t\n ");
-	if (pToken != NULL) {
-		osal_strtol(pToken, 16, &res);
-		y = (int)res;
-		GDL_LOGE("y = 0x%08x\n\r", y);
-	} else {
-		y = 3000;
-		/*efuse, register read write default value */
-		if (0x11 == x || 0x12 == x || 0x13 == x)
-			y = 0x80000000;
-	}
+		res = 0;
+		sub_len = strlen(pBuf);
+		GDL_LOGD("write parameter data = %s, len = %d", pBuf, sub_len);
+		if (sub_len != 0)
+			pToken = osal_strsep(&pBuf, "\t\n\r ");
+		if (sub_len != 0 && pToken != NULL) {
+			osal_strtol(pToken, 16, &res);
+			y = (int)res;
+		} else {
+			GDL_LOGW("y use default value");
+			break;
+		}
 
-	pToken = osal_strsep(&pBuf, "\t\n ");
-	if (pToken != NULL) {
-		osal_strtol(pToken, 16, &res);
-		z = (int)res;
-	} else {
-		z = 10;
-		/*efuse, register read write default value */
-		if (0x11 == x || 0x12 == x || 0x13 == x)
-			z = 0xffffffff;
-	}
+		res = 0;
+		sub_len = strlen(pBuf);
+		GDL_LOGD("write parameter data = %s, len = %d", pBuf, sub_len);
+		if (sub_len != 0)
+			pToken = osal_strsep(&pBuf, "\t\n\r ");
+		if (sub_len != 0 && pToken != NULL) {
+			osal_strtol(pToken, 16, &res);
+			z = (int)res;
+		} else {
+			GDL_LOGW("z use default value");
+			break;
+		}
+	} while (0);
+	GDL_LOGW("x = 0x%08x, y = 0x%08x, z = 0x%08x", x, y, z);
 
-	GDL_LOGI("x(0x%08x), y(0x%08x), z(0x%08x)\n\r", x, y, z);
-
-	/* For eng and userdebug load, have to enable wmt_dbg by
-	 * writing 0xDB9DB9 to * "/proc/driver/wmt_dbg" to avoid
+	/* For eng and userdebug load, have to enable gpsdl_dbg by
+	 * writing 0xDB9DB9 to * "/proc/driver/gpsdl_dbg" to avoid
 	 * some malicious use
 	 */
-#if 0
 	if (x == UNLOCK_MAGIC) {
-		dbgEnabled = 1;
+		gpsdl_dbg_enabled = true;
 		return len;
 	}
-#endif
+
+	if (!gpsdl_dbg_enabled) {
+		GDL_LOGW("please enable gpsdl_dbg firstly");
+		return len;
+	}
 
 	if (ARRAY_SIZE(g_gps_dl_proc_test_func_list) > x && NULL != g_gps_dl_proc_test_func_list[x])
-		(*g_gps_dl_proc_test_func_list[x]) (x, y, z);
+		(*g_gps_dl_proc_test_func_list[x])(y, z);
 	else
-		GDL_LOGW("no handler defined for command id(0x%08x)\n\r", x);
+		GDL_LOGW("no handler defined for command id, x = 0x%08x", x);
 
 	return len;
 }
--- data_link/linux/inc/gps_dl_procfs.h
+++ data_link/linux/inc/gps_dl_procfs.h
@@ -16,7 +16,7 @@
 int gps_dl_procfs_setup(void);
 int gps_dl_procfs_remove(void);
 
-typedef int(*gps_dl_procfs_test_func_type) (int par1, int par2, int par3);
+typedef int(*gps_dl_procfs_test_func_type)(int y, int z);
 
 #endif /* _GPS_DL_PROCFS_H */
 
