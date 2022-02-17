1b556dc2bc419042c7684ed2d5bf5e3764bac885
Hua Fu <hua.fu@mediatek.com>
2022-02-17 15:36:27 +0800

    [ALPS05241707] GPS: Remove unused coda_gen definition
    Remove unused coda_gen definition.
    
    MTK-Commit-Id: cdc34d617e4966ffce285a0130b3b2174d31b618
    
    Signed-off-by: Hua Fu <hua.fu@mediatek.com>
    Feature: Location Chipset Capability
    Change-Id: I5f8ec5f0d49c01c8ffe4b59d81fb077e2459bd10
    CR-Id: ALPS05241707

--- data_link/hw/inc/connac2_0/coda_gen/conn_infra/conn_rf_spi_mst_reg.h
+++ data_link/hw/inc/connac2_0/coda_gen/conn_infra/conn_rf_spi_mst_reg.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2019 MediaTek Inc.
+ * Copyright (c) 2020 MediaTek Inc.
  */
 #ifndef __CONN_RF_SPI_MST_REG_REGS_H__
 #define __CONN_RF_SPI_MST_REG_REGS_H__
@@ -8,17 +8,14 @@
 #define CONN_RF_SPI_MST_REG_BASE                               0x18004000
 
 #define CONN_RF_SPI_MST_ADDR_SPI_STA_ADDR                      (CONN_RF_SPI_MST_REG_BASE + 0x0000)
-
 #define CONN_RF_SPI_MST_ADDR_FM_CTRL_ADDR                      (CONN_RF_SPI_MST_REG_BASE + 0x000C)
-
+#define CONN_RF_SPI_MST_ADDR_FM_CTRL_ADDR                      (CONN_RF_SPI_MST_REG_BASE + 0x000C)
 #define CONN_RF_SPI_MST_ADDR_SPI_FM_ADDR_ADDR                  (CONN_RF_SPI_MST_REG_BASE + 0x0030)
 #define CONN_RF_SPI_MST_ADDR_SPI_FM_WDAT_ADDR                  (CONN_RF_SPI_MST_REG_BASE + 0x0034)
 #define CONN_RF_SPI_MST_ADDR_SPI_FM_RDAT_ADDR                  (CONN_RF_SPI_MST_REG_BASE + 0x0038)
-
 #define CONN_RF_SPI_MST_ADDR_SPI_GPS_GPS_ADDR_ADDR             (CONN_RF_SPI_MST_REG_BASE + 0x0210)
 #define CONN_RF_SPI_MST_ADDR_SPI_GPS_GPS_WDAT_ADDR             (CONN_RF_SPI_MST_REG_BASE + 0x0214)
 #define CONN_RF_SPI_MST_ADDR_SPI_GPS_GPS_RDAT_ADDR             (CONN_RF_SPI_MST_REG_BASE + 0x0218)
-
 
 
 #define CONN_RF_SPI_MST_REG_SPI_STA_FM_BUSY_ADDR               CONN_RF_SPI_MST_ADDR_SPI_STA_ADDR
