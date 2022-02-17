bcb19d902a1f424869fe1527d2d4cb1b4c1c2e9a
Tianfang Li <tianfang.li@mediatek.com>
2022-02-17 15:40:04 +0800

    [ALPS05681726] GPS: remove MTK_COMBO_CHIP macro
    remove MTK_COMBO_CHIP macro
    
    MTK-Commit-Id: 146ccb16cee341dc4c2b9d3d2f7eae490487c754
    
    Change-Id: I8fa02dbada36973b2d89087974db1c457c40064a
    Signed-off-by: Tianfang Li <tianfang.li@mediatek.com>
    CR-Id: ALPS05681726
    Feature: Location Chipset Capability

--- data_link/Android.mk
+++ data_link/Android.mk
@@ -7,14 +7,7 @@ LOCAL_MODULE := gps_drv_dl_$(GPS_PLATFORM).ko
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_OWNER := mtk
 LOCAL_INIT_RC := init.gps_drv.rc
-
-ifeq ($(MTK_COMBO_SUPPORT),yes)
 LOCAL_REQUIRED_MODULES := conninfra.ko
-else
-$(warning MTK_COMBO=$(MTK_COMBO_SUPPORT))
-$(warning gps_drv_dl.ko does not claim the requirement for conninfra.ko)
-endif
-
 include $(MTK_KERNEL_MODULE)
 $(warning GPS_PLATFORM=$(GPS_PLATFORM))
 GPS_OPTS := GPS_PLATFORM=$(GPS_PLATFORM)
--- gps_stp/Android.mk
+++ gps_stp/Android.mk
@@ -7,14 +7,7 @@ LOCAL_MODULE := gps_drv_stp.ko
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_OWNER := mtk
 LOCAL_INIT_RC := init.gps_drv.rc
-
-ifeq ($(MTK_COMBO_SUPPORT),yes)
 LOCAL_REQUIRED_MODULES := wmt_drv.ko
-else
-$(warning MTK_COMBO=$(MTK_COMBO_SUPPORT))
-$(warning gps_drv_stp.ko does not claim the requirement for wmt_drv.ko)
-endif
-
 include $(MTK_KERNEL_MODULE)
 
 endif
