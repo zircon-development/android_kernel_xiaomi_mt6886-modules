c4e0eb4b3e57429cfac7ab47b258d2eda2c7e8ca
Zhenyu Jing <zhenyu.jing@mediatek.com>
2022-08-21 14:01:24 +0800

    [ALPS07527592] GPS: Update EMI layout for MT6985 - V4 to V5
    GPS: Update EMI layout for MT6985 - V4 to V5
    
    MTK-Commit-Id: 439d67a3f5e9c357e3b2c2c930315a5ff57aa13a
    
    Signed-off-by: Zhenyu Jing <zhenyu.jing@mediatek.com>
    CR-Id: ALPS07527592
    Feature: Location Chipset Capability
    Change-Id: Ic7ce013bdb3cd753afe69b136e488b8f6ac0f2aa

--- gps_mcudl/inc/gps_mcudl_emi_layout.h
+++ gps_mcudl/inc/gps_mcudl_emi_layout.h
@@ -10,18 +10,18 @@ struct gps_mcudl_emi_layout {
 	unsigned char mcu_bin[0x020000];    /*0xF000_0000 ~ 0xF002_0000, 128KB*/
 	unsigned char gps_bin[0x032000];    /*0xF002_0000 ~ 0xF005_2000, 200KB*/
 	unsigned char dsp_bin[0x080000];    /*0xF005_2000 ~ 0xF00D_2000, 512KB*/
-	unsigned char mnl_bin[0x36B000];    /*0xF00D_2000 ~ 0xF043_D000, 3500KB*/
-	unsigned char mpe_bin[0x0C0000];    /*0xF043_D000 ~ 0xF04F_D000, 768KB*/
-	unsigned char mcu_ro_rsv[0x013000]; /*0xF04F_D000 ~ 0xF051_0000, 76KB*/
+	unsigned char mnl_bin[0x2EE000];    /*0xF00D_2000 ~ 0xF03C_0000, 3000KB*/
+	unsigned char mpe_bin[0x0];         /*0xF03C_0000 ~ 0xF03C_0000, 0KB*/
+	unsigned char mcu_ro_rsv[0x0];      /*0xF03C_0000 ~ 0xF03C_0000, 0KB*/
 
-	unsigned char mcu_gps_rw[0x07EC00]; /*0xF051_0000 ~ 0xF058_EC00, 507KB*/
-	unsigned char scp_batch[0x04B000];  /*0xF058_EC00 ~ 0xF05D_9C00, 300KB*/
-	unsigned char mcu_rw_rsv[0x016400]; /*0xF05D_9C00 ~ 0xF0F0_0000, 89KB*/
-	unsigned char mcu_share[0x040000];  /*0xF0F0_0000 ~ 0xF063_0000, 256KB*/
-	unsigned char gps_legacy[0x078000]; /*0xF063_0000 ~ 0xF0A8_8000, 480KB*/
-	unsigned char gps_nv_emi[0x0C0000]; /*0xF0A8_8000 ~ 0xF072_8000, 768KB*/
-	unsigned char gps_ap2mcu[0x004000]; /*0xF072_8000 ~ 0xF072_C000, 16KB*/
-	unsigned char gps_mcu2ap[0x004000]; /*0xF072_C000 ~ 0xF073_0000, 16KB*/
+	unsigned char mcu_gps_rw[0x085000]; /*0xF03C_0000 ~ 0xF044_5000, 532KB*/
+	unsigned char scp_batch[0x04B000];  /*0xF044_5000 ~ 0xF049_0000, 300KB*/
+	unsigned char mcu_rw_rsv[0x0];      /*0xF049_0000 ~ 0xF049_0000, 0KB*/
+	unsigned char mcu_share[0x040000];  /*0xF049_0000 ~ 0xF04D_0000, 256KB*/
+	unsigned char gps_legacy[0x078000]; /*0xF04D_0000 ~ 0xF054_8000, 480KB*/
+	unsigned char gps_nv_emi[0x0C0000]; /*0xF054_8000 ~ 0xF060_8000, 768KB*/
+	unsigned char gps_ap2mcu[0x004000]; /*0xF060_8000 ~ 0xF060_C000, 16KB*/
+	unsigned char gps_mcu2ap[0x004000]; /*0xF060_C000 ~ 0xF061_0000, 16KB*/
 };
 
 void gps_mcudl_show_emi_layout(void);
