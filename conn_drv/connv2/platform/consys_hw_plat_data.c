81a6377112f96a9ba8244146df01582553342e5e
Andrew-sh.Cheng <andrew-sh.cheng@mediatek.com>
2022-09-23 09:53:23 +0800

    [ALPS06878709] Conninfra: Add mt6983 security mode platform data
    Add mt6983 security mode platform data
    Change hw_ops as SMC call
    
    MTK-Commit-Id: 318238b8ebd439bf5af80f64682f9430fc68808f
    
    CR-Id: ALPS06878709
    Feature: conn_infra
    Signed-off-by: Andrew-sh.Cheng <andrew-sh.cheng@mediatek.com>
    Change-Id: I49032dd06c7ab84cbc1a27bfdabc356cb947a83c

--- conn_drv/connv2/platform/consys_hw_plat_data.c
+++ conn_drv/connv2/platform/consys_hw_plat_data.c
@@ -22,6 +22,7 @@ struct conninfra_plat_data __weak mt6877_plat_data = {};
 struct conninfra_plat_data __weak mt6879_plat_data = {};
 struct conninfra_plat_data __weak mt6895_plat_data = {};
 struct conninfra_plat_data __weak mt6983_plat_data = {};
+struct conninfra_plat_data __weak mt6983_plat_data_atf = {};
 
 #ifdef CONFIG_OF
 const struct of_device_id apconninfra_of_ids[] = {
@@ -47,6 +48,12 @@ const struct of_device_id apconninfra_of_ids[] = {
 		.compatible = "mediatek,mt6983-consys",
 	#if IS_ENABLED(CONFIG_MTK_COMBO_CHIP_CONSYS_6983)
 		.data = (void*)&mt6983_plat_data,
+	#endif
+	},
+	{
+		.compatible = "mediatek,mt6983-consys-atf",
+	#if IS_ENABLED(CONFIG_MTK_COMBO_CHIP_CONSYS_6983)
+		.data = (void*)&mt6983_plat_data_atf,
 	#endif
 	},
 	{
